e892a5d9489dc248d81be03411aa8b404dd28164
Abadon125 <tony@grosinger.net>
2010-12-17 18:30:33 -0800

    Clean up scripts

--- scripts/banner.js
+++ scripts/banner.js
@@ -1,34 +1,13 @@
-
-
-<!-- Copyright 2002 Bontrager Connection, LLC
-
-//
-
+<!--
 // Type the number of images you are rotating.
-
-
-
 NumberOfImagesToRotate = 6;
-
-
-
 // Specify the first and last part of the image tag. 
-
-
-
 FirstPart = '<img src="images/header';
-
 LastPart = '.jpg" height="158" width="960">';
 
-
-
 function printImage() {
-
-var r = Math.ceil(Math.random() * NumberOfImagesToRotate);
-
-document.write(FirstPart + r + LastPart);
-
+	var r = Math.ceil(Math.random() * NumberOfImagesToRotate);
+	document.write(FirstPart + r + LastPart);
 }
-
 //-->
 
