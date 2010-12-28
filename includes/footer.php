bc71e4bff97fb89647ffe053729c0ca04177d6b9
Abadon125 <tony@grosinger.net>
2010-12-27 20:30:58 -0800

    Added attribution

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
   <div class="footer">
-    <p align="center">Atrium Minecraft - <?php echo GetServerStatus("mc.grosinger.net","25565")?></p>
+    <p align="center">Atrium Minecraft - <?php echo GetServerStatus("mc.grosinger.net","25565")?>
+	<br />Created by <a href=http://grosinger.net>Tony Grosinger</a></p>
   </div>
   </div>
 </body>
